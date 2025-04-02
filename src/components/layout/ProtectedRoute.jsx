5beb39a91e46764fabd7815f7a098f6f69f947b8
Karan Singh <karans21.dev@gmail.com>
2025-04-02 16:21:32 +0530

    removed unwanted codes

--- src/components/layout/ProtectedRoute.jsx
+++ src/components/layout/ProtectedRoute.jsx
@@ -2,20 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-// const ProtectedRoute = ({ roles }) => {
-//   const { isAuthenticated, role } = useSelector((state) => state.auth);
-
-//   if (!isAuthenticated) {
-//     return <Navigate to="/" replace />;
-//   }
-
-//   if (roles && !roles.includes(role)) {
-//     return <Navigate to="/dashboard" replace />;
-//   }
-
-//   return <Outlet />;
-// };
-
 const ProtectedRoute = ({ roles }) => {
   const { isAuthenticated, role } = useSelector((state) => state.auth);
 
